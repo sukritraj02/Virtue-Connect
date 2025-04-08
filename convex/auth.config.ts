08aa505bdd471e4d9bae3cc19af5a9756c027beb
sukritraj02 <sukritt.raj@gmail.com>
2025-04-08 20:52:57 +0530

    initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://fleet-spaniel-24.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="m-10">
+      <SignInButton>
+        <Button>Sign In</Button>
+      </SignInButton>
+    </div>
+    );
+  }
