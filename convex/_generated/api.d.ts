a30853fce68b91f8b789e89c84edb1f295e9fab5
sukritraj02 <sukritt.raj@gmail.com>
2025-04-13 15:11:07 +0530

    interview.ts commit

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,7 @@ import type {
 } from "convex/server";
 import type * as comments from "../comments.js";
 import type * as http from "../http.js";
+import type * as interviews from "../interviews.js";
 import type * as users from "../users.js";
 
 /**
@@ -28,6 +29,7 @@ import type * as users from "../users.js";
 declare const fullApi: ApiFromModules<{
   comments: typeof comments;
   http: typeof http;
+  interviews: typeof interviews;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
