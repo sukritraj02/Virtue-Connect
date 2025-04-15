cdaf0f8059f22751ae4c57bf16a299671dafcfcc
sukritraj02 <sukritt.raj@gmail.com>
2025-04-16 01:37:31 +0530

    codeeditor done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "./globals.css";
-import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
+import {  RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import ConvexClerkProvider from "@/components/providers/ConvexClerkProvider";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -50,6 +52,7 @@ export default function RootLayout({
           <RedirectToSignIn />
         </SignedOut>
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   </ConvexClerkProvider>
